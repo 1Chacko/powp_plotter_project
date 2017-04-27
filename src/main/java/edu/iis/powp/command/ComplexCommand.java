1e96ab4710fb9a3148d3f58732fbbbcd6fbd1097
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-04-27 15:19:11 +0200

    3.1.2 Dodanie ComplexCommand

---
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -0,0 +1,32 @@
+package edu.iis.powp.command;
+
+import java.util.List;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class ComplexCommand implements IPlotterCommand
+{
+	private List<IPlotterCommand> lista;
+	
+	public List<IPlotterCommand> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<IPlotterCommand> lista) {
+		this.lista = lista;
+	}
+
+	public ComplexCommand(List<IPlotterCommand> lista) {
+		super();
+		this.lista = lista;
+	}
+	
+	@Override
+	public void execute(IPlotter driver) {
+		for(Object element : lista) {
+			((IPlotterCommand)element).execute(driver);
+		}
+		
+	}
+	
+}
